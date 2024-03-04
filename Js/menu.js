d0f5518fd1962a166647f118f5af982dea53fa3a
Farid Alfaro <farix6@gmail.com>
2024-03-04 20:13:52 -0300

    contacto fin

--- Js/menu.js
+++ Js/menu.js
@@ -1,61 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const bienvenida = document.querySelector('.bienvenida');
-    const menuBienvenida = document.querySelector('.menu-bienvenida');
-    const links = document.querySelectorAll('.bienvenida-link');
-    const linkss = document.querySelector('.bienvenida-links');
-    const closeBtn = document.querySelector('.close-btn');
-    const menus = document.querySelectorAll('.menu-bienvenida > div');
-    const header = document.querySelector('header');
-    const htmlBody = document.documentElement;
+  const bienvenida = document.querySelector('.bienvenida');
+  const menuBienvenida = document.querySelector('.menu-bienvenida');
+  const links = document.querySelectorAll('.bienvenida-link');
+  const linkss = document.querySelector('.bienvenida-links');
+  const closeBtn = document.querySelector('.close-btn');
+  const menus = document.querySelectorAll('.menu-bienvenida > div');
+  const htmlBody = document.documentElement;
 
-    closeBtn.addEventListener('click', function () {
+  closeBtn.addEventListener('click', function () {
+    menus.forEach(menu => {
+      menu.style.opacity = '0';
+      menu.style.display = 'none';
+      menu.classList.remove('menu-content');
+      menuBienvenida.style.transform = 'translateX(60vw)';
+      bienvenida.style.transform = 'translateX(0)';
+      linkss.style.transform = 'translateX(0)';
+
+      htmlBody.style.overflow = 'auto';
+    });
+  });
+
+  links.forEach(link => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
       menus.forEach(menu => {
         menu.style.opacity = '0';
         menu.style.display = 'none';
         menu.classList.remove('menu-content');
-        menuBienvenida.style.transform = 'translateX(60vw)';
-        bienvenida.style.transform = 'translateX(0)';
-        linkss.style.transform = 'translateX(0)';
-        header.style.display = '';
-
-        htmlBody.style.overflow = 'auto';
       });
-    });
 
-    links.forEach(link => {
-      link.addEventListener('click', function (event) {
-        event.preventDefault();
+      const targetMenuId = this.id.replace('menu', '').toLowerCase();
+      const targetMenu = document.querySelector(`.menu-${targetMenuId}`);
 
-        menus.forEach(menu => {
-          menu.style.opacity = '0';
-          menu.style.display = 'none';
-          menu.classList.remove('menu-content');
-        });
+      targetMenu.style.display = 'block';
 
-        const targetMenuId = this.id.replace('menu', '').toLowerCase();
-        const targetMenu = document.querySelector(`.menu-${targetMenuId}`);
+      setTimeout(() => {
+        targetMenu.classList.add('menu-content');
+        targetMenu.style.opacity = '1';
 
-        targetMenu.style.display = 'block';
-        header.style.display = 'none';
+        bienvenida.style.transform = 'translateX(-60vw)';
+        menuBienvenida.style.transform = 'translateX(0)';
+        linkss.style.transform = 'translateX(-60vw)';
 
-        setTimeout(() => {
-          targetMenu.classList.add('menu-content');
-          targetMenu.style.opacity = '1';
-
-          bienvenida.style.transform = 'translateX(-60vw)';
-          menuBienvenida.style.transform = 'translateX(0)';
-          linkss.style.transform = 'translateX(-60vw)';
-
-          // htmlBody.style.overflow = 'hidden';
-
-          if (window.scrollY !== 0) {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-          }
-        }, 10);
-      });
+        if (window.scrollY !== 0) {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+      }, 10);
     });
   });
-
+});
 
 document.addEventListener("DOMContentLoaded", function () {
     const cursor = document.createElement("div");
